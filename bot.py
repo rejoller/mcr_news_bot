55fd2fc898bc5987c97c8290637e37eedc36dcc8
rejoller <130197750+rejoller@users.noreply.github.com>
2024-04-28 11:14:42 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Router, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 
-BOT_TOKEN  = '6569068360:AAEUN1C31L78AAFwuztKdrM4q5xyeHBSuyk'
+BOT_TOKEN  = ''
 bot = Bot(token=BOT_TOKEN)
 
 router = Router()
